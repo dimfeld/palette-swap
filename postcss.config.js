ed564bc615068a07f841059022378f5201444726
Daniel Imfeld <daniel@danielimfeld.com>
2020-04-18 08:33:13 +0000

    Skip purgecss in webpack production

--- postcss.config.js
+++ postcss.config.js
@@ -1,34 +1,35 @@
-const production = !process.env.ROLLUP_WATCH;
+const production =
+  !process.env.ROLLUP_WATCH && process.env.NODE_ENV !== 'production';
 const purgecss = require('@fullhuman/postcss-purgecss');
 
 module.exports = {
-    plugins: [
-        require('postcss-import')(),
-        require('tailwindcss'),
-        require('autoprefixer'),
-        production &&
-            purgecss({
-                content: ['./**/*.html', './**/*.svelte'],
-                defaultExtractor: content => {
-                    const regExp = new RegExp(/[A-Za-z0-9-_:/]+/g);
+  plugins: [
+    require('postcss-import')(),
+    require('tailwindcss'),
+    require('autoprefixer'),
+    production &&
+      purgecss({
+        content: ['./**/*.html', './**/*.svelte'],
+        defaultExtractor: (content) => {
+          const regExp = new RegExp(/[A-Za-z0-9-_:/]+/g);
 
-                    const matchedTokens = [];
+          const matchedTokens = [];
 
-                    let match = regExp.exec(content);
-                    // To make sure that you do not lose any tailwind classes used in class directive.
-                    // https://github.com/tailwindcss/discuss/issues/254#issuecomment-517918397
-                    while (match) {
-                        if (match[0].startsWith('class:')) {
-                            matchedTokens.push(match[0].substring(6));
-                        } else {
-                            matchedTokens.push(match[0]);
-                        }
+          let match = regExp.exec(content);
+          // To make sure that you do not lose any tailwind classes used in class directive.
+          // https://github.com/tailwindcss/discuss/issues/254#issuecomment-517918397
+          while (match) {
+            if (match[0].startsWith('class:')) {
+              matchedTokens.push(match[0].substring(6));
+            } else {
+              matchedTokens.push(match[0]);
+            }
 
-                        match = regExp.exec(content);
-                    }
+            match = regExp.exec(content);
+          }
 
-                    return matchedTokens;
-                },
-            }),
-    ],
+          return matchedTokens;
+        },
+      }),
+  ],
 };
