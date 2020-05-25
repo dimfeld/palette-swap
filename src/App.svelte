9af1cb0c2a59b2188db68afde4e410421a48a45b
Daniel Imfeld <daniel@danielimfeld.com>
2020-05-25 01:45:48 +0000

    Save color in indexedDB. Hide top button

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
   import Pickr from './Pickr.svelte';
   import '@simonwep/pickr/dist/themes/classic.min.css';
+  import { set as setIdb, get as getIdb } from 'idb-keyval';
 
   const pickrOptions = {
     theme: 'classic',
@@ -10,10 +11,17 @@
     comparison: false,
     defaultRepresentation: 'RGBA',
     showAlways: true,
+    useAsButton: true,
+    // sliders: 'hv',
 
     components: {
       // Input / output Options
       interaction: {
+        hex: false,
+        rgba: false,
+        hsla: false,
+        hsva: false,
+        cmyk: false,
         clear: false,
         save: false,
       },
@@ -21,10 +29,30 @@
   };
 
   let currentColor = 'rgb(0.2,0.4,0.6)';
+
+  function colorToString(c) {
+    return currentColor.toRGBA
+      ? currentColor.toRGBA().toString()
+      : currentColor.toString();
+  }
+
+  getIdb('current-color')
+    .then((c) => {
+      console.log('loaded', c);
+      if (c) {
+        currentColor = c;
+      }
+    })
+    .catch((e) => {
+      console.error(e);
+    });
+
+  $: setIdb('current-color', colorToString(currentColor));
 </script>
 
 <Pickr
   class="flex flex-col items-center space-y-4"
   options={pickrOptions}
   bind:value={currentColor} />
-{currentColor.toRGBA ? currentColor.toRGBA() : currentColor}
+{colorToString(currentColor)}
+<div class="h-32 w-32" style="background-color:{colorToString(currentColor)}" />
